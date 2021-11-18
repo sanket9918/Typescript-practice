0a22ade9601a6c7461713c018caaf586d331c80b
sanket9918 <www.sanket1mohapatra@gmail.com>
2021-11-18 16:31:57 +0530

    Added express

---
+++ express/src/controllers/todos.ts
@@ -0,0 +1,46 @@
+import { RequestHandler } from "express";
+import { Todo } from "../models/todo";
+
+const TODOS:Todo[] = [];
+
+export const createTodo:RequestHandler = (req,res,next) => {
+    const text = (req.body as { text: string }).text;
+    
+    const newTodo = new Todo(Math.random().toString(), text)
+    
+    TODOS.push(newTodo);
+    res.status(201).json({message:"Todo created",createTodo:newTodo})
+}
+
+export const getTodo: RequestHandler = (req, res, next) => {
+    res.json({todos:TODOS})
+}
+
+export const updateTodo: RequestHandler<{id:string}> = (req, res, next) => {
+    const todoId = req.params.id
+
+    const updatedText = (req.body as { text: string }).text
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId)
+    
+    if (todoIndex < 0) {
+        throw new Error("No record found")
+    }
+
+    TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText)
+    res.json({message:"Todo updated",updateTodo:TODOS[todoIndex]})
+
+}
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+    const todoId = req.body.id
+
+   
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId)
+
+    if (todoIndex < 0) {
+        throw new Error("No record found")
+    }
+    TODOS.splice(todoIndex, 1)
+    res.json({message:"Todo deleted"})
+
+}
---
+++ express/src/routes/todos.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { createTodo, deleteTodo, getTodo, updateTodo } from '../controllers/todos';
+const router = Router();
+
+router.post('/', createTodo)
+router.get('/', getTodo)
+router.patch('/:id',updateTodo)
+router.delete('/',deleteTodo)
+
+export default router
